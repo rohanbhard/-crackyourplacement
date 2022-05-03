2efeab5595b7fdea7124d343eae6aa38d8f46395
rohanbhard <1929036@kiit.ac.in>
2022-05-03 11:24:33 +0530

    first day

---
+++ DAY 1/array_duplicate_number.cpp
@@ -0,0 +1,20 @@
+#leetcode code
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        vector<int> count(nums.size(), 0);
+        
+    for(int i=0;i < nums.size() ;i++)
+    {
+      count[nums[i]]++;
+    }
+    for(int i=0; i< nums.size();i++)
+    {
+        if(count[i] > 1)
+        {
+            return i;
+        }
+    }
+     return 0;   
+    }
+};
